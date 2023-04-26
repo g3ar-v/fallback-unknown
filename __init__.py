0d5f9c755bfd78273b24f9f828e5098f683f8451
victor <vfranktor@gmail.com>
2023-04-26 16:58:39 +0100

    refactor(__init__.py-&-dialog): change mycroft module to core

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mycroft.skills.core import FallbackSkill
+from core.skills.core import FallbackSkill
 
 
 class UnknownSkill(FallbackSkill):
